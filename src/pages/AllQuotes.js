88c8a25dc9ede77c46da69be350172c84f835573
Vahid Porki <ivahidp@gamil.com>
2023-01-07 13:55:57 +0330

    Load quote detail data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,12 +1,41 @@
+import { useEffect } from "react";
 import QuoteList from "../components/quotes/QuoteList";
-
-const QUOTES = [
-	{ id: "q1", author: "reza", text: "Learning React is fun!" },
-	{ id: "q2", author: "vahid", text: "Never Give Up!" },
-];
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
+// const QUOTES = [
+// 	{ id: "q1", author: "reza", text: "Learning React is fun!" },
+// 	{ id: "q2", author: "vahid", text: "Never Give Up!" },
+// ];
 
 const AllQuotes = () => {
-	return <QuoteList quotes={QUOTES}></QuoteList>;
+	const {
+		sendRequest,
+		status,
+		data: loadedQuotes,
+		error,
+	} = useHttp(getAllQuotes, true);
+
+	useEffect(() => {
+		sendRequest();
+	}, [sendRequest]);
+
+	if (status === "pending") {
+		return (
+			<div className="centered">
+				<LoadingSpinner></LoadingSpinner>
+			</div>
+		);
+	}
+	if (error) {
+		<p className="centered focused">{error}</p>;
+	}
+
+	if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
+		return <NoQuotesFound></NoQuotesFound>;
+	}
+	return <QuoteList quotes={loadedQuotes}></QuoteList>;
 };
 
 export default AllQuotes;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,29 +1,49 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 import Comments from "../components/comments/Comments";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
-const QUOTES = [
-	{ id: "q1", author: "reza", text: "Learning React is fun!" },
-	{ id: "q2", author: "vahid", text: "Never Give Up!" },
-];
 const QuoteDetail = () => {
 	const match = useRouteMatch();
 	const params = useParams();
-	const quote = QUOTES.find((quote) => quote.id === params.quoteId);
-	console.log(match);
-	if (!quote) return <p>No Quote Found!</p>;
+	const { quoteId } = params;
+	const {
+		sendRequest,
+		status,
+		data: loadedQuote,
+		error,
+	} = useHttp(getSingleQuote);
+
+	useEffect(() => {
+		sendRequest(quoteId);
+	}, [sendRequest, quoteId]);
+
+	if (status === "pending") {
+		return (
+			<div className="centered">
+				<LoadingSpinner />
+			</div>
+		);
+	}
+	if (error) {
+		<p className="centered ">{error}</p>;
+	}
+
+	if (!loadedQuote) return <p>No Quote Found!</p>;
 
 	return (
 		<Fragment>
 			<HighlightedQuote
-				text={quote.text}
-				author={quote.author}
+				text={loadedQuote.text}
+				author={loadedQuote.author}
 			></HighlightedQuote>
 			<Route path={match.path} exact>
 				<div className="centered">
-					<Link className="btn--flat" to={`${match.url}/comments`}>
+					<Link className="btns--flat" to={`${match.url}/comments`}>
 						Load Comments
 					</Link>
 				</div>
