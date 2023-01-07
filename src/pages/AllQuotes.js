c6da82537c13286f39f5f959d5c7279f8a739db4
Vahid Porki <ivahidp@gamil.com>
2023-01-07 09:58:07 +0330

    Added quote detail and not found page

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,11 @@
+import QuoteList from "../components/quotes/QuoteList";
+const QUOTES = [
+	{ id: "q1", author: "reza", text: "Learning React is fun!" },
+	{ id: "q2", author: "vahid", text: "Never Give Up!" },
+];
+
 const AllQuotes = () => {
-	return <h1>All Quotes Page</h1>;
+	return <QuoteList quotes={QUOTES}></QuoteList>;
 };
 
 export default AllQuotes;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,20 +1,30 @@
-import { Fragment } from 'react';
-import { useParams, Route } from 'react-router-dom';
+import { Fragment } from "react";
+import { useParams, Route } from "react-router-dom";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
-import Comments from '../components/comments/Comments';
+import Comments from "../components/comments/Comments";
 
+const QUOTES = [
+	{ id: "q1", author: "reza", text: "Learning React is fun!" },
+	{ id: "q2", author: "vahid", text: "Never Give Up!" },
+];
 const QuoteDetail = () => {
-  const params = useParams();
+	const params = useParams();
+	const quote = QUOTES.find((quote) => quote.id === params.quoteId);
 
-  return (
-    <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
-        <Comments />
-      </Route>
-    </Fragment>
-  );
+	if (!quote) return <p>No Quote Found!</p>;
+
+	return (
+		<Fragment>
+			<HighlightedQuote
+				text={quote.text}
+				author={quote.author}
+			></HighlightedQuote>
+			<Route path={`/quotes/${params.quoteId}/comments`}>
+				<Comments />
+			</Route>
+		</Fragment>
+	);
 };
 
 export default QuoteDetail;
