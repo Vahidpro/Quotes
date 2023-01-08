1913c9d522801ed4918c98d330352b8a726be897
Vahid Porki <ivahidp@gamil.com>
2023-01-08 09:24:47 +0330

    Migrated to react-router v6

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,27 +1,33 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
+import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
-  return (
-    <header className={classes.header}>
-      <div className={classes.logo}>Great Quotes</div>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <NavLink to='/quotes' activeClassName={classes.active}>
-              All Quotes
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/new-quote' activeClassName={classes.active}>
-              Add a Quote
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	return (
+		<header className={classes.header}>
+			<div className={classes.logo}>Quotes</div>
+			<nav className={classes.nav}>
+				<ul>
+					<li>
+						<NavLink
+							to="/quotes"
+							className={(navData) => (navData.isActive ? classes.active : "")}
+						>
+							All Quotes
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to="/new-quote"
+							className={(navData) => (navData.isActive ? classes.active : "")}
+						>
+							Add a Quote
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 };
 
 export default MainNavigation;
